ae8779c927532556b7299e2a53582b41309e7e9c
Gioi.Pronesti <gioi.pronesti@gmail.com>
2024-11-19 16:03:01 +0100

    add: express server and main root

--- app.js
+++ app.js
@@ -13,3 +13,18 @@ Provare a restituire un singolo post dalla rotta show, sempre in formato json
 Consigli:
 partite registrando tutte le rotte direttamente nel file app.js come abbiamo fatto in classe in modo da concentrarsi prima sulla creazione delle rotte per capire bene come funziona l’architettura REST
 una volta che tutte le rotte funzionano (assicuratevi di averle tutte testate con Postman) allora passate al refactoring sfruttando il Router di Express come indicato nella consegna.*/
+
+const express = require("express"); // richiamo express
+const app = express(); // definisco l'applicativo dove vive express
+const port = 3000; // definisco la porta per accedere al server
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.console.log("questa è la root");
+  res.send("root, questa è la risposta del server");
+});
+
+app.listen(port, () => {
+  console.log(`server in ascolto sulla porta ${port}`);
+});
